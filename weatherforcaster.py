96c4650587f80b0ea805abd6f16af69cf1130521
kidistchekol <108240460+kidistchekol@users.noreply.github.com>
2022-06-26 17:43:07 -0400

    Create weatherforcaster.py

---
+++ weatherforcaster.py
@@ -0,0 +1,13 @@
+ef forcastweather(weather):
+
+    if weather == 'cloudy':
+        print("its cloudy")
+
+    elif weather == 'sunny':
+        print('it is nice out')
+    elif weather == 'hot':
+         print('it is hot')
+    elif weather == 'snowy':
+        print('build a snowman')
+    else:
+        print("i don't now what to say")
